d9067e3966eaa6b4097cb057c073add84c9ad745
tqyao <tqyao@foxmail.com>
2024-04-16 15:50:45 +0800

    ElementUI 配置，按需引入，国际化

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -8,7 +8,7 @@ module.exports = {
     browser: true,
     es2021: true,
     node: true,
-    jest: true,
+    jest: true
   },
   /* 指定如何解析语法 */
   parser: 'vue-eslint-parser',
@@ -19,15 +19,15 @@ module.exports = {
     parser: '@typescript-eslint/parser',
     jsxPragma: 'React',
     ecmaFeatures: {
-      jsx: true,
-    },
+      jsx: true
+    }
   },
   /* 继承已有的规则 */
   extends: [
     'eslint:recommended',
     'plugin:vue/vue3-essential',
     'plugin:@typescript-eslint/recommended',
-    'plugin:prettier/recommended',
+    'plugin:prettier/recommended'
   ],
   plugins: ['vue', '@typescript-eslint'],
   /*
@@ -35,13 +35,45 @@ module.exports = {
    * "warn" 或 1   ==>  打开的规则作为警告（不影响代码执行）
    * "error" 或 2  ==>  规则作为一个错误（代码不能执行，界面报错）
    */
+  // rules: {
+  //   // eslint（https://eslint.bootcss.com/docs/rules/）
+  //   'no-var': 'error', // 要求使用 let 或 const 而不是 var
+  //   'no-multiple-empty-lines': ['warn', { max: 1 }], // 不允许多个空行
+  //   'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+  //   'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+  //   'no-unexpected-multiline': 'warn', // 禁止空余的多行
+  //   'no-useless-escape': 'off', // 禁止不必要的转义字符
+  //
+  //   // typeScript (https://typescript-eslint.io/rules)
+  //   '@typescript-eslint/no-unused-vars': 'error', // 禁止定义未使用的变量
+  //   '@typescript-eslint/prefer-ts-expect-error': 'error', // 禁止使用 @ts-ignore
+  //   '@typescript-eslint/no-explicit-any': 'off', // 禁止使用 any 类型
+  //   '@typescript-eslint/no-non-null-assertion': 'off',
+  //   '@typescript-eslint/no-namespace': 'off', // 禁止使用自定义 TypeScript 模块和命名空间。
+  //   '@typescript-eslint/semi': 'off',
+  //
+  //   // eslint-plugin-vue (https://eslint.vuejs.org/rules/)
+  //   'vue/multi-word-component-names': 'off', // 要求组件名称始终为 “-” 链接的单词
+  //   'vue/script-setup-uses-vars': 'error', // 防止<script setup>使用的变量<template>被标记为未使用
+  //   'vue/no-mutating-props': 'off', // 不允许组件 prop的改变
+  //   'vue/attribute-hyphenation': 'off', // 对模板中的自定义组件强制执行属性命名样式
+
+  // },
+  root: true,
+  extends: [
+    'plugin:vue/vue3-essential',
+    'eslint:recommended',
+    '@vue/eslint-config-prettier/skip-formatting'
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest'
+  },
   rules: {
-    // eslint（https://eslint.bootcss.com/docs/rules/）
     'no-var': 'error', // 要求使用 let 或 const 而不是 var
     'no-multiple-empty-lines': ['warn', { max: 1 }], // 不允许多个空行
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-unexpected-multiline': 'error', // 禁止空余的多行
+    'no-unexpected-multiline': 'warn', // 禁止空余的多行
     'no-useless-escape': 'off', // 禁止不必要的转义字符
 
     // typeScript (https://typescript-eslint.io/rules)
@@ -57,5 +89,32 @@ module.exports = {
     'vue/script-setup-uses-vars': 'error', // 防止<script setup>使用的变量<template>被标记为未使用
     'vue/no-mutating-props': 'off', // 不允许组件 prop的改变
     'vue/attribute-hyphenation': 'off', // 对模板中的自定义组件强制执行属性命名样式
+    'prettier/prettier': [
+      'warn',
+      {
+        singleQuote: true, // 单引号
+        semi: false, // 无分号
+        printWidth: 80, // 每行宽度至多80字符
+        trailingComma: 'none', // 不加对象|数组最后逗号
+        endOfLine: 'auto' // 换行符号不限制（win mac 不一致）
+      }
+    ],
+    'vue/multi-word-component-names': [
+      'warn',
+      {
+        ignores: ['index'] // vue组件名称多单词组成（忽略index.vue）
+      }
+    ],
+    'vue/no-setup-props-destructure': ['off'], // 关闭 props 解构的校验
+    // 💡 添加未定义变量错误提示，create-vue@3.6.3 关闭，这里加上是为了支持下一个章节演示。
+    'no-undef': 'error',
+    // 仅在此目录覆盖默认设置
+    eqeqeq: 'warn',
+    strict: 'off'
   },
+  globals: {
+    ElMessage: 'readonly',
+    ElMessageBox: 'readonly',
+    ElLoading: 'readonly'
+  }
 }
--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,17 @@
 <script setup lang="ts">
-const str = '123'
+import zhCn from 'element-plus/es/locale/lang/zh-cn'
 </script>
 
 <template>
-  <h1>哈哈</h1>
+  <!-- 国际化  -->
+  <el-config-provider :locale="zhCn">
+    <div>
+      <el-config-provider>
+        <el-table mb-1 :data="[]" />
+        <el-pagination :total="100" />
+      </el-config-provider>
+    </div>
+  </el-config-provider>
 </template>
 
 <style scoped></style>
