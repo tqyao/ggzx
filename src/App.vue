94990b877d0aff0bc562cc63ef011a925cba691f
tqyao <tqyao@foxmail.com>
2024-04-17 10:00:46 +0800

    login、home、404 路由初步配置

--- src/App.vue
+++ src/App.vue
@@ -1,58 +1,11 @@
 <script setup lang="ts">
 import zhCn from 'element-plus/es/locale/lang/zh-cn'
-import axios from 'axios'
-import { reqLogin, reqUserInfo } from '@/api/user'
-import { onMounted } from 'vue'
-
-onMounted(async () => {
-  console.log('onMounted...')
-  const res = await reqUserInfo()
-  console.log(res)
-
-  const res2 = await reqLogin({
-    username: 'admin',
-    password: '111111'
-  })
-  console.log(res2)
-})
-
-console.log(import.meta.env)
-axios({
-  method: 'POST',
-  url: '/api/user/login',
-  data: {
-    username: 'admin',
-    password: '111111'
-  }
-})
 </script>
 
 <template>
-  <!-- 国际化  -->
-  <div>
-    <h1>hello world</h1>
-  </div>
   <el-config-provider :locale="zhCn">
-    <div>
-      <el-config-provider>
-        <el-table mb-1 :data="[]" />
-        <el-pagination :total="100" />
-      </el-config-provider>
-    </div>
-
-    <hr />
-    <svg style="width: 100px; height: 100px">
-      <use xlink:href="#icon-phone" fill="yellow"></use>
-    </svg>
-    <hr />
-    <svg-icon name="home" width="200px" height="300px" color="green"></svg-icon>
+    <router-view></router-view>
   </el-config-provider>
 </template>
 
-<style scoped lang="scss">
-div {
-  h1 {
-    color: $base-color;
-  }
-}
-</style>
+<style scoped lang="scss"></style>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,15 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import { constantRoute } from '@/router/router'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: []
+  history: createWebHistory(),
+  routes: constantRoute,
+  scrollBehavior() {
+    return {
+      top: 0,
+      left: 0
+    }
+  }
 })
 
 export default router
